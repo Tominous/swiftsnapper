415cf168f107be7299f223557254e5f8ab81842b
Tominous <18189490+Tominous@users.noreply.github.com>
2019-09-18 19:44:58 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,14 @@ gulp.task('default', ['typescript', 'scss']);
 gulp.task('typescript', (callback) => {
     var sourceTsFiles = ['./scripts/**/*.ts', './typings/'];
     var tsResult = gulp.src(sourceTsFiles)
-        //.pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe(tsc(tscProj));
 
     tsResult.dts
         .pipe(gulp.dest(output + 'scripts'));
 
     return tsResult.js
-        //.pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(output + 'scripts'));
 });
 gulp.task('scss', () => {
